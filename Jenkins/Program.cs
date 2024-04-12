3fb771c627bfbd5a2b2907aee97c24615b974ee4
4eever <napnap0929@gmail.com>
2024-04-13 01:32:39 +0300

    Repository Initialization

---
+++ Jenkins/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paraschanka");
+            Console.WriteLine("Nikolai");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
